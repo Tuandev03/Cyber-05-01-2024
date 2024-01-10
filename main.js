e8cab586a60f6f00eb5f431d98b6658538e83b41
Your Name <you@example.com>
2024-01-10 11:10:02 +0700

    update

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 var $ = document.querySelector.bind(document);
 var $$ = document.querySelectorAll.bind(document);
 
-// Tính tiền lương
 var btnClick1 = $(".txt-btn1");
 var btnClick2 = $(".txt-btn2");
 var btnClick3 = $(".txt-btn3");
 var btnClick4 = $(".txt-btn4");
 var btnClick5 = $(".txt-btn5");
+// Mô hình 3 khối trong Tính tiền lương
+/*
+* Đầu vào: Người dùng nhập vào ô input
+* Các bước xử lý: 
+  ** Bước 1: Khởi tạo biến ô input của tienLuong,soNgay và nút button của btnClick1 
+  ** Bước 2: DOM tới HTML và gán cho biến tienLuong,soNgay, btnClick1
+  ** Bước 3: Tính khi người dùng nhấp vào button thì có công thức sau: tienLuong * soNgay
+  ** Bước 4: In ra kết quả 
+
+* Đầu ra: Hiển thị kết quả trên màn hình giao diện bằng innerHTML 
+  */
 // Tính tiền lương
 btnClick1.onclick = () => {
   var tienLuong = $("#txt-tienLuongNgay ").value * 1;
@@ -18,7 +28,15 @@ btnClick1.onclick = () => {
     "vn-VN"
   ).format(tienLuong * soNgay)} VND`;
 };
-
+//  Mô hình 3 khối tính giá trị trung bình.
+/**
+ * Đầu vào: Người dùng nhập vào ô input.
+ * Xử lý:
+ * Bước 1: Khởi tạo biến cho 5 ô input và nút button DOM tới HTML gán cho 5 biến đó.
+ * Bước 2: Khi người dùng nhấp vào button ta lấy 5 biến vừa khởi tạo cộng lại và chia cho 5.
+ * Bước 3: In ra kết quả
+ * Đầu ra: Hiển thị kết quả trên màn hình giao diện bằng innerHTML
+ */
 // Tính giá trị trung bình
 btnClick2.onclick = () => {
   var num1 = $("#txt-num1").value * 1;
@@ -31,6 +49,16 @@ btnClick2.onclick = () => {
     (num1 + num2 + num3 + num4 + num5) / 5
   }`;
 };
+
+//  Mô hình 3 khối quy đổi tiền.
+/**
+ * Đầu vào: người dùng nhập vào ô input
+ * Xử lý;
+ * ** Bước 1 :Khởi tạo biến cho 1 ô input và button và DOM tới HTML gán cho biến đó
+ * ** Bước 2:  Khi người dùng nhấp vào button ta lấy biến vừa tạo * 23500
+ * ** Bước 3: In ra kết quả biến trên
+ * Đầu ra:  Hiển thị kết quả trên màn hình giao diện bằng innerHTML
+ */
 // Quy đổi tiền
 btnClick3.onclick = () => {
   var money = $("#txt-money").value * 1;
@@ -39,7 +67,15 @@ btnClick3.onclick = () => {
   ketQua.innerHTML = ` <i class="fa-solid fa-circle-arrow-right fs-5 text-light"></i>
 ${new Intl.NumberFormat("vn-VN").format(money * 23500)} VNĐ `;
 };
-
+//  Mô hình tính diện tích, chu vi hình chữ nhật
+/**
+ * Đầu vào: người dùng nhập vào ô input
+ * Xử lý;
+ * ** Bước 1 :Khởi tạo biến cho ô input chieuDai và chieuRong và button và DOM tới HTML gán cho biến đó
+ * ** Bước 2:  Khi người dùng nhấp vào button ta lấy biến (chieuDai + chieuRong) * 2
+ * ** Bước 3: In ra kết quả biến trên
+ * Đầu ra:  Hiển thị kết quả trên màn hình giao diện bằng innerHTML
+ */
 // Tính diện tích, chu vi hình chữ nhật
 btnClick4.onclick = () => {
   var chieuDai = $("#txt-chieuDai").value * 1;
@@ -49,11 +85,19 @@ btnClick4.onclick = () => {
     chieuDai * chieuRong
   }; Chu vi: ${(chieuDai + chieuRong) * 2}`; //
 };
+
+//  Mô hình tính Tính tổng 2 ký số
+/**
+ * Đầu vào: người dùng nhập vào ô input
+ * Xử lý;
+ * ** Bước 1 :Khởi tạo biến cho ô input và button và DOM tới HTML gán cho biến đó
+ * ** Bước 2:  Khi người dùng nhấp vào button ta thực hiện lấy hàng trăm và hàng chục như sau Math.floor((num % 100) / 10) + (num % 10)
+ * ** Bước 3: In ra kết quả biến trên
+ * Đầu ra:  Hiển thị kết quả trên màn hình giao diện bằng innerHTML
+ */
 // Tính tổng 2 ký số
 btnClick5.onclick = () => {
   var num = $("#txt-number").value * 1;
-  var hangTram = 0;
-  var hangChuc = 0;
 
   var ketQua = $(".txt-ketQua5");
   ketQua.innerHTML = `
@@ -74,5 +118,5 @@ inputForm.forEach(function (input) {
     // Cập nhật giá trị của input chỉ bao gồm các số
     event.target.value = numericValue;
   });
-});
+}); 
 // ${(Math.floor(num / 10) + Math.floor((num % 100) / 10) + 1)}
